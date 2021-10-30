d31b60cc29475ae8703f7dd0586c44ffcc928558
Uzair Iqbal <uzair.qq@outlook.com>
2021-10-30 13:30:03 +0500

    Created Basic Schema in Server.js File

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
+import {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString
+} from 'graphql';
 
 
 const app = express();
 const PORT = 4002;
 
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'Message',
+        fields: () => ({
+            messageName: {
+                type: GraphQLString,
+                resolve: () => 'My Name Is Uzair'
+            }
+        })
+
+    })
+})
+
 app.use('/graphql', graphqlHTTP({
+    schema: schema,
     graphiql: true
 }))
 
